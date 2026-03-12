bde7e68e08886293bed426bdc175edb66c10171a
Shivendra Pratap <shivendrapratapsp3@gmail.com>
2026-03-13 01:55:07 +0530

    Cleanup: Remove redundant legacy files and directories, restoring clean V2 architecture

--- errors.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-
-def page_not_found(e):
-    return render_template('errors/404.html'), 404
-
-def internal_error(e):
-    return render_template('errors/500.html'), 500
--- routes/__init__.py
+++
@@ -1,2 +0,0 @@
-# routes/__init__.py
-# This file makes the directory a Python package
